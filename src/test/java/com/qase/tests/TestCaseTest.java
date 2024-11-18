0f98b0a20b69018ff0c1b4fceb6345a9de1d268c
Olga <olka030389@yandex.ru>
2024-11-18 12:14:02 +0300

    updated tests

--- src/test/java/com/qase/tests/TestCaseTest.java
+++ src/test/java/com/qase/tests/TestCaseTest.java
@@ -1,6 +1,5 @@
 package com.qase.tests;
 
-import com.qase.enums.*;
 import com.qase.model.Project;
 import com.qase.model.TestCase;
 import com.qase.other.TestData;
@@ -14,44 +13,24 @@ import static org.testng.Assert.*;
 
 public class TestCaseTest extends BaseTest {
 
-    @DataProvider(name = "methods provider")
-    public Object[][] methodsProvider() {
+    @DataProvider(name = "data")
+    public Object[][] getDataFromDataProvider() {
         return new Object[][]{
-
-                {Status.ACTUAL.getName(), Severity.MINOR.getName(), Priority.MEDIUM.getName(),
-                        Type.ACCEPTANCE.getName(), Layer.API.getName(), IsFlaky.YES.getName(),
-                        Behavior.DESTRUCTIVE.getName(), AutomationStatus.AUTOMATED.getName()},
-                {Status.DEPRECATED.getName(), Severity.BLOCKER.getName(), Priority.LOW.getName(),
-                        Type.COMPATIBILITY.getName(), Layer.E2E.getName(), IsFlaky.NO.getName(),
-                        Behavior.POSITIVE.getName(), AutomationStatus.MANUAL.getName()},
-                {Status.DRAFT.getName(), Severity.CRITICAL.getName(), Priority.HIGH.getName(),
-                        Type.FUNCTIONAL.getName(), Layer.UNIT.getName(), IsFlaky.YES.getName(),
-                        Behavior.NEGATIVE.getName(), AutomationStatus.AUTOMATED.getName()},
-                {Status.ACTUAL.getName(), Severity.MAJOR.getName(), Priority.MEDIUM.getName(),
-                        Type.OTHER.getName(), Layer.API.getName(), IsFlaky.YES.getName(),
-                        Behavior.DESTRUCTIVE.getName(), AutomationStatus.AUTOMATED.getName()},
-                {Status.DRAFT.getName(), Severity.NORMAL.getName(), Priority.HIGH.getName(),
-                        Type.EXPLORATORY.getName(), Layer.UNIT.getName(), IsFlaky.YES.getName(),
-                        Behavior.NEGATIVE.getName(), AutomationStatus.AUTOMATED.getName()},
-                {Status.DEPRECATED.getName(), Severity.TRIVIAL.getName(), Priority.LOW.getName(),
-                        Type.INTEGRATION.getName(), Layer.E2E.getName(), IsFlaky.NO.getName(),
-                        Behavior.POSITIVE.getName(), AutomationStatus.MANUAL.getName()},
-                {Status.ACTUAL.getName(), Severity.MAJOR.getName(), Priority.LOW.getName(),
-                        Type.REGRESSION.getName(), Layer.E2E.getName(), IsFlaky.NO.getName(),
-                        Behavior.POSITIVE.getName(), AutomationStatus.MANUAL.getName()},
-                {Status.DRAFT.getName(), Severity.CRITICAL.getName(), Priority.HIGH.getName(),
-                        Type.SECURITY.getName(), Layer.UNIT.getName(), IsFlaky.YES.getName(),
-                        Behavior.NEGATIVE.getName(), AutomationStatus.AUTOMATED.getName()},
-                {Status.ACTUAL.getName(), Severity.MAJOR.getName(), Priority.MEDIUM.getName(),
-                        Type.SMOKE.getName(), Layer.E2E.getName(), IsFlaky.NO.getName(),
-                        Behavior.DESTRUCTIVE.getName(), AutomationStatus.AUTOMATED.getName()},
-                {Status.DEPRECATED.getName(), Severity.BLOCKER.getName(), Priority.LOW.getName(),
-                        Type.PERFORMANCE.getName(), Layer.E2E.getName(), IsFlaky.NO.getName(),
-                        Behavior.POSITIVE.getName(), AutomationStatus.MANUAL.getName()}
+                {"Actual", "Minor", "Medium", "Acceptance", "API", "Yes", "Destructive", "Automated"},
+                {"Deprecated", "Blocker", "Low", "Compatibility", "E2E", "No", "Positive", "Manual"},
+                {"Draft", "Critical", "High", "Functional", "Unit", "Yes", "Negative", "Automated"},
+                {"Actual", "Major", "Medium", "Other", "API", "Yes", "Destructive", "Automated"},
+                {"Draft", "Normal", "High", "Exploratory", "Unit", "Yes", "Negative", "Automated"},
+                {"Deprecated", "Trivial", "Low", "Integration", "E2E", "No", "Positive", "Manual"},
+                {"Actual", "Major", "Low", "Regression", "API", "E2E", "No", "Positive", "Manual"},
+                {"Draft", "Critical", "High", "Security", "Unit", "Yes", "Negative", "Automated"},
+                {"Actual", "Major", "Medium", "Smoke", "E2E", "Yes", "Destructive", "Automated"},
+                {"Deprecated", "Blocker", "Low", "Performance", "E2E", "No", "Positive", "Manual"},
+                {"Actual", "Major", "Medium", "Usability", "API", "Yes", "Destructive", "Automated"},
         };
     }
 
-    @Test(dataProvider = "methods provider", description = "Check the added test case with valid data")
+    @Test(dataProvider = "data", description = "Check the added test case with different data")
     public void testCaseShouldBeCreated(String status, String severity, String priority, String type,
                                         String layer, String isFlaky, String behavior, String autoStatus) {
         Project project = getProject();
@@ -86,9 +65,8 @@ public class TestCaseTest extends BaseTest {
     @Test(description = "Check the updated test case with valid data")
     public void testCaseShouldBeUpdated() {
         Project project = getProject();
-        TestCase testCase = getTestCase(Status.ACTUAL.getName(), Severity.CRITICAL.getName(),
-                Priority.HIGH.getName(), Type.ACCEPTANCE.getName(), Layer.API.getName(),
-                IsFlaky.YES.getName(), Behavior.POSITIVE.getName(), AutomationStatus.MANUAL.getName());
+        TestCase testCase = getTestCase("Actual", "Major", "Medium",
+                "Usability", "API", "Yes", "Destructive", "Automated");
         TestCase testCaseUpdated = getUpdatedTestCase();
         loginPage.openPage()
                 .isPageOpened()
@@ -112,9 +90,8 @@ public class TestCaseTest extends BaseTest {
     @Test(description = "Check the deletion of the existing suit")
     public void testCaseShouldBeDeleted() {
         Project project = getProject();
-        TestCase testCase = getTestCase(Status.DEPRECATED.getName(), Severity.MINOR.getName(),
-                Priority.MEDIUM.getName(), Type.FUNCTIONAL.getName(), Layer.UNIT.getName(),
-                IsFlaky.YES.getName(), Behavior.DESTRUCTIVE.getName(), AutomationStatus.AUTOMATED.getName());
+        TestCase testCase = getTestCase("Actual", "Major", "Medium",
+                "Usability", "API", "Yes", "Destructive", "Automated");
         loginPage.openPage()
                 .isPageOpened()
                 .logIn(testData.USER, testData.PASS);
